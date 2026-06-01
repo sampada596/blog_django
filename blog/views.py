566a45fc75d374413046603fe2cb60605fe7d84b
sampada596 <sampadaranabhat@gmail.com>
2026-06-01 21:50:10 +0545

    admin

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.filter(is_published=True).order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
