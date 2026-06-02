413620112d8e08b80f072838d71549a481269185
sampada596 <sampadaranabhat@gmail.com>
2026-06-02 17:18:32 +0545

    added user authentication

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('register/',views.register, name='register'),
+    path('login/',views.user_login, name='login'),
+    path('logout/',views.user_logout, name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Post
+from django.contrib.auth import login , logout , authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm 
+from django.shortcuts import redirect
 
 def home(request):
     posts = Post.objects.filter(is_published=True).order_by('-created_at')
     return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
