b26d91bfce20a4a23e822aaa041ba8aa9ca24293
sampada596 <sampadaranabhat@gmail.com>
2026-08-09 16:43:40 +0545

    fixed database config for production

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -69,10 +70,10 @@ WSGI_APPLICATION = 'blog_django.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
