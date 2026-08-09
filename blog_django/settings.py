42bb870baaf5079296b7b42444464b982a698274
sampada596 <sampadaranabhat@gmail.com>
2026-08-09 16:53:51 +0545

    fixed database config

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -69,12 +69,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog_django.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(
-        default=str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
-}
+import os
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
